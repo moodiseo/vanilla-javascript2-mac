f0ad3b59a8a6dadea41836a8c579ad0abfa98e38
김다빈 <135933231+slqkealr@users.noreply.github.com>
2023-10-17 00:01:46 +0900

    The Canvas API

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -3,5 +3,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = "800";
 canvas.height = "800";
 
+ctx.fillRect(210 - 40,200 - 50,15,100);
+ctx.fillRect(350 - 40,200 - 50,15,100);
+ctx.fillRect(260 - 40,200 - 50,60,200);
 
-ctx.fillRect(50,50,100,200);
+ctx.arc(250,100,50,0,2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(265,80,8,Math.PI,2 * Math.PI);
+ctx.arc(235,80,8,Math.PI,2 * Math.PI);
+ctx.fill();   
